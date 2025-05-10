91581b576a111b370df92341f45c969833370674
rae8108 <146367231+rae8108@users.noreply.github.com>
2025-05-10 16:13:55 +0100

    created new components

---
+++ src/components/Earth/Earth.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function Earth() {
+  return (
+    <div>
+      <h1>
+        This is Earth page
+        </h1>
+    </div>
+  )
+}
+
+export default Earth
---
+++ src/components/Missions/Missions.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function Missions() {
+  return (
+    <div>
+      <h1>
+        This is missions page
+        </h1>
+    </div>
+  )
+}
+
+export default Missions
